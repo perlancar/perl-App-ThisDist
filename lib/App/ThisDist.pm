ed309abe4ee2aef1fe5f513a4a4a907c0c83908d
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-10-02 11:11:27 +0700

    Initial commit

---
+++ lib/App/ThisDist.pm
@@ -0,0 +1,15 @@
+package App::ThisDist;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: This CPAN {distribution,module,author,...}
+
+=head1 SEE ALSO
+
+See included scripts:
+
+# INSERT_EXECS_LIST
