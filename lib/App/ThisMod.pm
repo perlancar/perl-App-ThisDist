28ca579425ae49bbfb5349ac537b7bc9a575f396
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2022-06-05 15:40:06 +0700

    Add empty module App::ThisMod

---
+++ lib/App/ThisMod.pm
@@ -0,0 +1,20 @@
+package App::ThisMod;
+
+use strict;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Synonym for App::ThisDist
+
+=head1 DESCRIPTION
+
+This is an empty module to let users find App::ThisDist more easily.
+
+
+=head1 SEE ALSO
+
+L<App::ThisDist>
